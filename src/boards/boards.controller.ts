1385e6fa305e52596ee300ffa7ec137ce238f8f6
ricepotato <sukjun40@naver.com>
2024-12-10 19:39:09 +0900

    create board

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { BoardsService } from './boards.service';
 import { Board } from './boards.model';
 
@@ -9,5 +9,10 @@ export class BoardsController {
   @Get('/')
   getAllBoard(): Board[] {
     return this.boardService.getAllBoards();
+  }
+
+  @Post('/')
+  createBoard(@Body('title') title, @Body('description') description): Board {
+    return this.boardService.createBoard(title, description);
   }
 }
